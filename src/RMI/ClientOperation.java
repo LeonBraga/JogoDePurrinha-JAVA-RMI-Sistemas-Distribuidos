0b500a6120e79cf3b547c7b0578fb873484592a4
LeonBraga <leonathanbraga@id.uff.br>
2019-06-20 17:20:58 -0300

    Incluido validacoes e escolha de servidores em cliente

--- src/RMI/ClientOperation.java
+++ src/RMI/ClientOperation.java
@@ -9,28 +9,62 @@ public class ClientOperation {
     
     public static void main(String[] args) {
         try{
-        	RMIInterface mensageiro = (RMIInterface)Naming.lookup("//192.168.1.6/mensageiro");
-//        	RMIInterface mensageiro = (RMIInterface) Naming.lookup(RMIInterfaceImp.getURI());
-            Scanner scanner = new Scanner(System.in);
+        	
+        	String servidor1 = "localhost";
+        	String servidor2 = "localhost";
+        	String servidorV = "";
+        	String servidor = "localhost";
+        	String opt="0";
+        	
+        	Scanner scanner = new Scanner(System.in);
+        	
+        	while(!(opt.matches("1|2|3"))) {
+	        	System.out.println("Digite 1 para conectar em servidor A");
+	        	System.out.println("Digite 2 para conectar em servidor B");
+	        	System.out.println("Ou digite a senha secreta para habilitar um novo servidor:");
+	        	opt = scanner.nextLine();
+	        	if (!(opt.matches("1|2|n"))) {
+	        		System.out.println("Opcao invalida...");
+	        	}
+        	}
+        	
+        	if(opt.equals("1")) {
+        		servidor = servidor1;        		
+        	}
+        	else if(opt.equals("2")) {
+        		servidor = servidor2;       		
+        	}
+        	else if(opt.equals("n")) {
+        		System.out.println("Digite o IP para se conectar: ");
+        		servidorV = scanner.nextLine();
+        		servidor = servidorV;        		
+        	}
+        	else {
+        		System.out.println("Erro! Servidor nao localizado em cliente....");
+        		System.out.println("Iniciado em localhost");
+        	}
+        	
+        	RMIInterface mensageiro = (RMIInterface)Naming.lookup("//"+servidor+"/mensageiro");
+//        	RMIInterface mensageiro = (RMIInterface) Naming.lookup(RMIInterfaceImp.getURI());        	
             
             while (true) {//enquanto para sempre                
-//                mensagem = scanner.nextLine();//recebe o digitado
-//                mensageiro.enviarMensagem(mensagem);//manda pro servidor
-                
+            	
                 String meuNumeroJogador = "0";
                 String contadorJogador = "0";
                 
-                String nome;
-                String jogada;
-                String palpite;
+                String nome = "";
+                String jogada = "";
+                String palpite = "";
                 
-                System.out.println("Digite o nome do jogador: ");
-                nome = scanner.nextLine();
+                while(nome.equals("")) {
+                	System.out.println("Digite o nome do jogador: ");
+                	nome = scanner.nextLine();
+                }
                 
                 mensageiro.cadastraJogador(nome);                	
                 
                 meuNumeroJogador = mensageiro.getNumeroJogador(nome);
-                System.out.println("Meu Numero de Jogador: "+meuNumeroJogador);
+                System.out.println("Seu Numero de Jogador: "+ meuNumeroJogador);
                 
                 if (meuNumeroJogador.equals("1")) {
                 	System.out.println("Iniciando J1");
@@ -45,8 +79,14 @@ public class ClientOperation {
                 	//j2 cadastrou nome, fazendo jogada de j1
                 	if (contadorJogador.equals("2")) {       
                 		System.out.println("...");
-                        System.out.println("Digite o sua jogada 1, 2 ou 3: ");
-                        jogada = scanner.nextLine();
+                		while(jogada.equals("")) {
+                			System.out.println("Digite o sua jogada 0, 1, 2 ou 3: ");
+                			jogada = scanner.nextLine();
+                			if (!(jogada.matches("0|1|2|3"))) {
+                				System.out.println("Jogada invalida!");
+                				jogada = "";
+                			}
+                		}
                         mensageiro.fazerJogada1(nome,jogada);
                 	}
                 	
@@ -61,8 +101,14 @@ public class ClientOperation {
                 	//j2 fez jogada, j1 fazendo palpite
                   	if (contadorJogador.equals("4")) {       
                   		System.out.println("...");
-                        System.out.println("Digite o seu palpite (2-6): ");
-                        palpite = scanner.nextLine();
+                  		while (palpite.equals("")) {
+                  			System.out.println("Digite o seu palpite (0-6): ");
+                            palpite = scanner.nextLine();
+                 			if (!(palpite.matches("0|1|2|3|4|5|6"))) {
+                 				System.out.println("Palpite invalido!");
+                 				palpite = "";
+                 			}
+                  		}
                         mensageiro.fazerPalpite1(nome,palpite);
                 	}
                 	
@@ -89,8 +135,14 @@ public class ClientOperation {
                 	//j1 fez jogada, fazendo jogada de j2
                 	if (contadorJogador.equals("3")) { 
                 		System.out.println("...");
-                        System.out.println("Digite o sua jogada 1, 2 ou 3: ");
-                        jogada = scanner.nextLine();
+                		while(jogada.equals("")) {
+                			System.out.println("Digite o sua jogada 0, 1, 2 ou 3: ");
+                			jogada = scanner.nextLine();
+                			if (!(jogada.matches("0|1|2|3"))) {
+                				System.out.println("Jogada invalida!");
+                				jogada = "";
+                			}
+                		}
                         mensageiro.fazerJogada2(nome,jogada);
                 	}
                 	
@@ -106,9 +158,21 @@ public class ClientOperation {
                 	//j1 fez palpite, j2 fazendo palpite
                   	if (contadorJogador.equals("5")) {   
                   		System.out.println("----------------------");
-                  		System.out.println("O palpite do Jogador 1 foi: "+ mensageiro.getPalpite1());
-                        System.out.println("Digite o seu palpite (2-6): ");
-                        palpite = scanner.nextLine();
+                  		String palpite1 = mensageiro.getPalpite1();
+                  		while(palpite.equals("")) {
+                  			System.out.println("O palpite do Jogador 1 foi: "+ palpite1);
+                            System.out.println("Digite o seu palpite : ");
+                            palpite = scanner.nextLine();
+                            if (palpite.equals(palpite1)) {
+                            	System.out.println("Palpite invalido!");
+                            	System.out.println("Seu palpite nÃ£o pode ser igual ao do jogador 1!");
+                 				palpite = "";
+                            }
+                            else if (!(palpite.matches("0|1|2|3|4|5|6"))) {
+                				System.out.println("Palpite invalido!");
+                				palpite = "";
+                			}
+                  		}
                         mensageiro.fazerPalpite2(nome,palpite);
                 	}
                 	     	
@@ -116,31 +180,31 @@ public class ClientOperation {
                 }   
                 
                 contadorJogador = mensageiro.getContador();
-                if (contadorJogador.equals("6") || contadorJogador.equals("0")) {                		
+                if (contadorJogador.equals("6") || contadorJogador.equals("7")) {                		
                     String resultado = mensageiro.saberResultado(nome);
                 	System.out.println("-------RESULTADO------");
                 	System.out.println("----------------------");
 
-                    if (resultado=="0") {
+                    if (resultado.equals("0")) {
                     	System.out.println("Jogada J1: "+ mensageiro.getJogada1() + "/ Palpite J1: "+ mensageiro.getPalpite1());
                     	System.out.println("Jogada J2: "+ mensageiro.getJogada2() + "/ Palpite J2: "+ mensageiro.getPalpite2());
-                    	System.out.println("Resultado: "+ Integer.parseInt(mensageiro.getJogada1()) + Integer.parseInt(mensageiro.getJogada2()));
+                    	System.out.println("Resultado: "+ (Integer.parseInt(mensageiro.getJogada1()) + Integer.parseInt(mensageiro.getJogada2())));
                     	System.out.println("Empate, nenhum jogador acertou o palpite.");
                     }
-                    else if (resultado==meuNumeroJogador) {
+                    else if (resultado.equals(meuNumeroJogador)) {
                     	System.out.println("Jogada J1: "+ mensageiro.getJogada1() + "/ Palpite J1: "+ mensageiro.getPalpite1());
                     	System.out.println("Jogada J2: "+ mensageiro.getJogada2() + "/ Palpite J2: "+ mensageiro.getPalpite2());
-                    	System.out.println("Resultado: "+ Integer.parseInt(mensageiro.getJogada1()) + Integer.parseInt(mensageiro.getJogada2()));
-                    	System.out.println("Parabéns! Você acertou o palpite e venceu!");
+                    	System.out.println("Resultado: "+ (Integer.parseInt(mensageiro.getJogada1()) + Integer.parseInt(mensageiro.getJogada2())));
+                    	System.out.println("ParabÃ©ns! VocÃª acertou o palpite e venceu!");
                     }
                     else {
                     	System.out.println("Jogada J1: "+ mensageiro.getJogada1() + "/ Palpite J1: "+ mensageiro.getPalpite1());
                     	System.out.println("Jogada J2: "+ mensageiro.getJogada2() + "/ Palpite J2: "+ mensageiro.getPalpite2());
-                    	System.out.println("Resultado: "+ Integer.parseInt(mensageiro.getJogada1()) + Integer.parseInt(mensageiro.getJogada2()));
+                    	System.out.println("Resultado: "+ (Integer.parseInt(mensageiro.getJogada1()) + Integer.parseInt(mensageiro.getJogada2())));
                     	System.out.println("Derrota! Seu oponente acertou o palpite.");
                     }
                     System.out.println("----------------------");
-                    System.out.println("----------------------");
+                    System.out.println("----------------------");                 
             	}
             }
             
